cb7a937ea52fdc5eb2441b77b7864e39deace982
Codecamp <Codecamp@Codecamp-Mac-mini-2.local>
2017-02-22 09:37:24 +0100

    black chat bubble added! Custom cell added

---
+++ objc-starter/FriendlyChatObjC/MessageCellTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MessageCellTableViewCell.h
+//  FriendlyChatObjC
+//
+//  Created by Codecamp on 21.02.17.
+//  Copyright © 2017 Ibrahim Ulukaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageCellTableViewCell : UITableViewCell
+
+@end
