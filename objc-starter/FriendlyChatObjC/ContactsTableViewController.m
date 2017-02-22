a1903f0e8fb62eb98ab4a86b98646f605565c236
Codecamp <Codecamp@Codecamp-Mac-mini-2.local>
2017-02-22 15:52:42 +0100

    added addGroup function

--- objc-starter/FriendlyChatObjC/ContactsTableViewController.m
+++ objc-starter/FriendlyChatObjC/ContactsTableViewController.m
@@ -120,26 +120,35 @@ __weak ContactsTableViewController *weakViewController;
     self._contactsTableView.delegate = self;
     self._contactsTableView.dataSource = self;
     
-    [self._contactsTableView setNeedsDisplay];
-}
-
-
-- (BOOL) emailAvailable:(NSString *)email {
-    for (NSDictionary *dict in _allUsers) {
-        if ([dict[@"email"] isEqualToString: email]) {
-            return true;
-        }
-    }
-    return false;
+    [self createGroup:@"firstGroup"];
     
+    [self._contactsTableView setNeedsDisplay];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
-- (void) addUserToGroup: (NSDictionary *) userDict withGroupID: (NSString *) groupID {
-    [[[[[_ref child:@"groups"] child:groupID] child:@"user"] child:userDict[@"id"] ]setValue:@{@"joined": [self getCurrentTime]}];
+
+-(void) addUserToGroup: (NSDictionary *) userDict withGroupID: (NSString *) groupID {
+    [self addUserToGroup:userDict withGroupID:groupID withRights:@"default"];
+}
+
+- (void) addUserToGroup: (NSDictionary *) userDict withGroupID: (NSString *) groupID withRights: (NSString*)rights{
+    [[[[[_ref child:@"groups"] child:groupID] child:@"user"] child:userDict[@"id"] ]setValue:@{@"joined": [self getCurrentTime], @"rights": rights}];
+}
+
+
+- (void) createGroup :(NSString *) name{
+    NSString *newGroupID = [[_ref child:@"groups"] childByAutoId].key;
+    
+    [[[_ref child:@"groups"] child:newGroupID] setValue:@{@"created": [self getCurrentTime], @"name":name}];
+    
+    FIRUser *user = [FIRAuth auth].currentUser;
+    
+    NSDictionary *userDict = @{@"id" : user.uid, @"username" : user.displayName, @"email" : user.email};
+    
+    [self addUserToGroup:userDict withGroupID:newGroupID withRights:@"Admin"];
 }
 
 
